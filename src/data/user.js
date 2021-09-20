fc17bbae019ac894debfad93a2bfe82f2028c42a
vineetk242000 <vineetkumar242000@gmail.com>
2021-09-20 17:07:33 +0530

    added link to resume

--- src/data/user.js
+++ src/data/user.js
@@ -2,7 +2,7 @@ export const user = {
     name:"Vineet Kumar",
     short_intro:"A self-taught Full stack and a Flutter Developer based in Dhanbad (IN). I love building apps and websites that can solve real-world problems, and add value to society.",
     email:"vineetkumar242000@gmail.com",
-    resume:"resume/resume.pdf",
+    resume:"https://docs.google.com/document/d/1AVuQ7dKzWoKZEpghcSv0_bNWO6BXTkaOpsgAoEMg4Q8/edit?usp=sharing",
     about:
     `Hey! It's Vineet here.  I am currently in the 3rd year of my Undergrad course at Bengal College of Engineering and Technology, Durgapur. <br />This love and hate kind of relationship with product development started back in 2019  when I was trying to figure out what I'm good at.
     In a short period, I learned a great deal about development and life in general. 20+ Personal Projects later, I got an opportunity to work as a MERN Stack developer in one of the startups where I built an eCommerce website from scratch. <br />
