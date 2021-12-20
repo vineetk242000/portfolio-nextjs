7de9f3a7ab9abf75bcb70b9c33c73ca10f91d785
vineetk242000 <vineetkumar242000@gmail.com>
2021-12-20 22:18:18 +0530

    updated my resume link

--- src/data/user.js
+++ src/data/user.js
@@ -1,19 +1,20 @@
 export const user = {
-    name:"Vineet Kumar",
-    short_intro:"A self-taught Full stack and a Flutter Developer based in Dhanbad (IN). I love building apps and websites that can solve real-world problems, and add value to society.",
-    email:"vineetkumar242000@gmail.com",
-    resume:"https://docs.google.com/document/d/1AVuQ7dKzWoKZEpghcSv0_bNWO6BXTkaOpsgAoEMg4Q8/edit?usp=sharing",
-    about:
-    `Hey! It's Vineet here.  I am currently in the 3rd year of my Undergrad course at Bengal College of Engineering and Technology, Durgapur. <br />This love and hate kind of relationship with product development started back in 2019  when I was trying to figure out what I'm good at.
+  name: "Vineet Kumar",
+  short_intro:
+    "A self-taught Full stack and a Flutter Developer based in Dhanbad (IN). I love building apps and websites that can solve real-world problems, and add value to society.",
+  email: "vineetkumar242000@gmail.com",
+  resume:
+    "https://drive.google.com/file/d/16sMkfVvX9ppdyNy8HRrnt6jpt9TkNpx6/view?usp=sharing",
+  about: `Hey! It's Vineet here.  I am currently in the 3rd year of my Undergrad course at Bengal College of Engineering and Technology, Durgapur. <br />This love and hate kind of relationship with product development started back in 2019  when I was trying to figure out what I'm good at.
     In a short period, I learned a great deal about development and life in general. 20+ Personal Projects later, I got an opportunity to work as a MERN Stack developer in one of the startups where I built an eCommerce website from scratch. <br />
     Apart from these, I am really interested in the tech space. Nothing excites me more than the new tech innovations and gadgets. You can also find me writing Tech Blogs in my spare time. <br />
     And Yeah! Feel free to contact me for any Business or Project related queries. Maybe, I have the solution that you are looking for.`,
-    image:'img/me.jpeg',
-    social:{
-        github:'https://github.com/vineetk242000',
-        facebook:'https://www.facebook.com/vineet.thakur.1806253/',
-        instagram:'https://www.instagram.com/vineet_kumar01/',
-        linkdin:'https://www.linkedin.com/in/vineet-kumar-28151a200',
-        twitter:'https://twitter.com/VineetK16372803'
-    }
-}
+  image: "img/me.jpeg",
+  social: {
+    github: "https://github.com/vineetk242000",
+    facebook: "https://www.facebook.com/vineet.thakur.1806253/",
+    instagram: "https://www.instagram.com/vineet_kumar01/",
+    linkdin: "https://www.linkedin.com/in/vineet-kumar-28151a200",
+    twitter: "https://twitter.com/VineetK16372803",
+  },
+};
